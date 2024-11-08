ba47de75f751f484fa27bc862002653ab5bb4bc0
pichu1298 <62480135+pichu1298@users.noreply.github.com>
2024-11-08 12:16:42 -0500

    ad

--- vite-project/JS/main.js
+++ vite-project/JS/main.js
@@ -49,6 +49,17 @@ for (let i = 0; i < petCareItems.length; i++) {
   );
 }
 
+const dogItems = petCareItems.filter((item) => item.animal.includes("Dog"));
+const hamsterItems = petCareItems.filter((item) =>
+  item.animal.includes("Hamster")
+);
+const catItems = petCareItems.filter((item) => item.animal.includes("Cat"));
+const birdItems = petCareItems.filter((item) => item.animal.includes("Bird"));
+const rabbitItems = petCareItems.filter((item) =>
+  item.animal.includes("Rabbit")
+);
+const fishItems = petCareItems.filter((item) => item.animal.includes("Fish"));
+
 // const dog = petCareItems.map((dog) => dog.animal);
 
 // const dog = petCareItems.filter(animalDog);
@@ -59,29 +70,222 @@ for (let i = 0; i < petCareItems.length; i++) {
 
 // // petCareItems.filter((dog) => petCareItems.animal.includes("Dog"));
 
-// function createdSorted() {
-//   document.querySelector(".container").innerHtml = "";
-//   //load new array
-// }
+function createdSorted() {
+  document.querySelector(".container").innerHTML = "";
+  //load new array
+}
 
-// let dogButton = document.querySelector("#dogButton");
-// dogButton.addEventListener("click", function (event) {
-//   event.preventDefault();
-//   createdSorted();
-//   DOMSelectors.container.insertAdjacentHTML(
-//     "beforeend",
-//     `<div class = "card">
-//   <h1>${dog.title}</h1>
-//   <img src="${dog.image_url}" alt="${dog.alt_text}">
-//   <ul>
-//     <li>Price: $${dog.price} </li>
-//     <li>Store(s): ${dog.stores}</li>
-//     <li>Animal(s): ${dog.animal}</li>
-//   </ul>
-//   </div>`
-//   );
-// });
+let dogButton = document.querySelector("#dogButton");
+dogButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  createdSorted();
+  dogItems.forEach((item) => {
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `<div class = "card">
+    <h1>${item.title}</h1>
+    <img src="${item.image_url}" alt="${item.alt_text}">
+    <ul>
+      <li>Price: $${item.price} </li>
+      <li>Store(s): ${item.stores}</li>
+      <li>Animal(s): ${item.animal}</li>
+    </ul>
+    </div>`
+    );
+  });
+});
 
+let catButton = document.querySelector("#catButton");
+catButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  createdSorted();
+  catItems.forEach((item) => {
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `<div class = "card">
+    <h1>${item.title}</h1>
+    <img src="${item.image_url}" alt="${item.alt_text}">
+    <ul>
+      <li>Price: $${item.price} </li>
+      <li>Store(s): ${item.stores}</li>
+      <li>Animal(s): ${item.animal}</li>
+    </ul>
+    </div>`
+    );
+  });
+});
+
+let hamsterButton = document.querySelector("#hamsterButton");
+hamsterButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  createdSorted();
+  hamsterItems.forEach((item) => {
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `<div class = "card">
+    <h1>${item.title}</h1>
+    <img src="${item.image_url}" alt="${item.alt_text}">
+    <ul>
+      <li>Price: $${item.price} </li>
+      <li>Store(s): ${item.stores}</li>
+      <li>Animal(s): ${item.animal}</li>
+    </ul>
+    </div>`
+    );
+  });
+});
+
+let birdButton = document.querySelector("#birdButton");
+birdButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  createdSorted();
+  birdItems.forEach((item) => {
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `<div class = "card">
+    <h1>${item.title}</h1>
+    <img src="${item.image_url}" alt="${item.alt_text}">
+    <ul>
+      <li>Price: $${item.price} </li>
+      <li>Store(s): ${item.stores}</li>
+      <li>Animal(s): ${item.animal}</li>
+    </ul>
+    </div>`
+    );
+  });
+});
+
+let rabbitButton = document.querySelector("#rabbitButton");
+rabbitButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  createdSorted();
+  rabbitItems.forEach((item) => {
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `<div class = "card">
+    <h1>${item.title}</h1>
+    <img src="${item.image_url}" alt="${item.alt_text}">
+    <ul>
+      <li>Price: $${item.price} </li>
+      <li>Store(s): ${item.stores}</li>
+      <li>Animal(s): ${item.animal}</li>
+    </ul>
+    </div>`
+    );
+  });
+});
+
+let fishButton = document.querySelector("#fishButton");
+fishButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  createdSorted();
+  fishItems.forEach((item) => {
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `<div class = "card">
+    <h1>${item.title}</h1>
+    <img src="${item.image_url}" alt="${item.alt_text}">
+    <ul>
+      <li>Price: $${item.price} </li>
+      <li>Store(s): ${item.stores}</li>
+      <li>Animal(s): ${item.animal}</li>
+    </ul>
+    </div>`
+    );
+  });
+});
+
+const fromPetco = petCareItems.filter((item) => item.stores.includes("Petco"));
+
+let petcoButton = document.querySelector("#petco");
+petcoButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  createdSorted();
+  fromPetco.forEach((item) => {
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `<div class = "card">
+    <h1>${item.title}</h1>
+    <img src="${item.image_url}" alt="${item.alt_text}">
+    <ul>
+      <li>Price: $${item.price} </li>
+      <li>Store(s): ${item.stores}</li>
+      <li>Animal(s): ${item.animal}</li>
+    </ul>
+    </div>`
+    );
+  });
+});
+
+const fromAmazon = petCareItems.filter((item) =>
+  item.stores.includes("Amazon")
+);
+
+let amazonButton = document.querySelector("#amazon");
+amazonButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  createdSorted();
+  fromAmazon.forEach((item) => {
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `<div class = "card">
+    <h1>${item.title}</h1>
+    <img src="${item.image_url}" alt="${item.alt_text}">
+    <ul>
+      <li>Price: $${item.price} </li>
+      <li>Store(s): ${item.stores}</li>
+      <li>Animal(s): ${item.animal}</li>
+    </ul>
+    </div>`
+    );
+  });
+});
+
+const fromChewy = petCareItems.filter((item) => item.stores.includes("Chewy"));
+
+let chewyButton = document.querySelector("#chewy");
+chewyButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  createdSorted();
+  fromChewy.forEach((item) => {
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `<div class = "card">
+    <h1>${item.title}</h1>
+    <img src="${item.image_url}" alt="${item.alt_text}">
+    <ul>
+      <li>Price: $${item.price} </li>
+      <li>Store(s): ${item.stores}</li>
+      <li>Animal(s): ${item.animal}</li>
+    </ul>
+    </div>`
+    );
+  });
+});
+
+const fromPetSmart = petCareItems.filter((item) =>
+  item.stores.includes("PetSmart")
+);
+
+let petSmartButton = document.querySelector("#petSmart");
+petSmartButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  createdSorted();
+  fromPetSmart.forEach((item) => {
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `<div class = "card">
+    <h1>${item.title}</h1>
+    <img src="${item.image_url}" alt="${item.alt_text}">
+    <ul>
+      <li>Price: $${item.price} </li>
+      <li>Store(s): ${item.stores}</li>
+      <li>Animal(s): ${item.animal}</li>
+    </ul>
+    </div>`
+    );
+  });
+});
 // coldColor = document.querySelector("#coldColor");
 
 // coldColor.addEventListener("submit", function (coldColorFunction) {
